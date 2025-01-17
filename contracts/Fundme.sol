31fa5f82091596d2f027698c5b8524be795ae14e
patinnojoe <innocentjosiah57@gmail.com>
2025-01-18 00:28:15 +0100

    working with price feeds

--- contracts/Fundme.sol
+++ contracts/Fundme.sol
@@ -6,8 +6,12 @@ contract  Fundme{
     uint256 minimumUSD = 50;
     function send() public payable{
         require(msg.value >=5, "Cant send less than 50USD");
-        
-
-
+    
     }
+
+    function getPrice() public{
+        // addreess 0x694AA1769357215DE4FAC081bf1f309aDC325306
+    }
+
+     function getConversionRate()public{}
 }
